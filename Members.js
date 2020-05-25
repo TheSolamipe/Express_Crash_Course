670597704829222af05d572ebb43df84bbc8f317
TheSolamipe <akinolasolamipe@gmail.com>
2020-05-25 17:53:44 +0100

    Intial commit

---
+++ Members.js
@@ -0,0 +1,23 @@
+//Array of members
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Emeka Okon",
+    email: "emeka@gmail.com",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "Tara Banks",
+    email: "tara@gmail.com",
+    status: "inactive",
+  },
+];
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const logger = require("./middleware/logger");
+const members = require("./Members");
+
+const app = express();
+
+//Init middleware
+app.use(logger);
+
+//Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Homepage Route
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Member App",
+    members,
+  })
+);
+
+//Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//Members API route
+app.use("/api/members", require("./routes/api/members"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
